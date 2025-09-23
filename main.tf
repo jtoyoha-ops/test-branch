cab80b5f737f3553d6bffd8b1dadfb0ad95c2498
jtoyoha-ops <jtoyoha@gmail.com>
2025-09-23 17:36:04 +0900

    AL2023化

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2023
   instance_type = "t3.micro"
 
   tags = {
